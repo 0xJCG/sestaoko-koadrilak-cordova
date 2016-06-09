001e5b5f5d7de9bb754ea34c6852d2186c56a617
0xJCG <jonathancastrogonzalez@gmail.com>
2016-06-09 23:15:10 +0200

    Routes updated and unused files deleted..

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,59 +1,43 @@
 angular.module('app.routes', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
+    .state('sestaokoKoadrilak.program', {
+        url: '/program',
+        views: {
+            'menu': {
+                templateUrl: 'templates/program.html',
+                controller: 'programCtrl'
+            }
+        }
+    })
 
-      .state('sestaokoKoadrilak.program', {
-    url: '/page1',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/program.html',
-        controller: 'programCtrl'
-      }
-    }
-  })
+    .state('sestaokoKoadrilak.groups', {
+        url: '/groups',
+        views: {
+            'menu': {
+                templateUrl: 'templates/groups.html',
+                controller: 'groupsCtrl'
+            }
+        }
+    })
 
-  .state('sestaokoKoadrilak.groups', {
-    url: '/page2',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/groups.html',
-        controller: 'groupsCtrl'
-      }
-    }
-  })
+    .state('sestaokoKoadrilak.bars', {
+        url: '/bars',
+        views: {
+            'menu': {
+                templateUrl: 'templates/bars.html',
+                controller: 'barsCtrl'
+            }
+        }
+    })
 
-  .state('sestaokoKoadrilak.bars', {
-    url: '/page3',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/bars.html',
-        controller: 'barsCtrl'
-      }
-    }
-  })
+    .state('sestaokoKoadrilak', {
+        url: '/menu',
+        templateUrl: 'templates/sestaokoKoadrilak.html',
+        abstract: true
+    })
 
-  .state('sestaokoKoadrilak', {
-    url: '/side-menu21',
-    templateUrl: 'templates/sestaokoKoadrilak.html',
-    abstract:true
-  })
-
-  .state('page', {
-    url: '/page4',
-    templateUrl: 'templates/page.html',
-    controller: 'pageCtrl'
-  })
-
-$urlRouterProvider.otherwise('/side-menu21/page1')
-
-  
-
+    $urlRouterProvider.otherwise('/program')
 });
